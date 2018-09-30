a4ea936c1ed9ea6401482ca1c7f6862e919c335c
Trevor Hegert <thegert138@gmail.com>
2018-09-30 17:50:09 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,7 +3,7 @@ def valid_move?(board, index)
   if board[index].to_i <= 8 && board[index].to_i >= 0 && position_taken?(board, index) == false
     true
   elsif board[index].to_i > 8 || board[index].to_i < 0
-    false
+    nil
   end
 end
 
